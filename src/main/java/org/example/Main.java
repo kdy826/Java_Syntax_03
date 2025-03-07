b6eb7bd2aa88ddeff4ca204ac6c511ddc1520e95
kdy826 <kimdy66695@gmail.com>
2025-03-07 16:51:02 +0900

    커밋메세지9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,77 +5,62 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+      boolean[] arr1 = new boolean[3];
+      arr1[0] = true;
+      arr1[1] = false;
+      arr1[2] = true;
 
-     int a = 10;
+        System.out.println();
 
-     int[]arr = new int[5];
-//     arr[0] = 10;
-//     arr[1] = 20;
-//     arr[2] = 30;
-
-        for(int b = 0; b< arr.length; b ++){
-            arr[b] = (b+1) *10;
-
-        }
-
-//     int sum = arr[0] +arr[1] + arr[2];
-     int sum = 0;
-     for (int i = 0 ; i< arr.length; i++){
-         sum += arr[i];
-     }
-     int avg = sum / arr.length;
-//        System.out.println(arr[0]);
-//        System.out.println(arr[1]);
-//        System.out.println(arr[2]);
-//        System.out.printf("첫번째 : %d , 두번째 : %d,세번째 : %d, 총합 : %d" ,arr[0] ,arr[1] , arr[2] , arr[0] +arr[1] + arr[2]);
-        System.out.println("총합"+ sum);
-        System.out.println("평균" + avg);
 
     }
 }
 
+//int a = 10;
+//
+//     int[]arr;  // 정수들 int[] 모양으로 만들어진 객체의 리모컨만 들어갈수있는 arr라는 변수를 만든다.
+//     arr = new int[5]; //arr라는 변수에 int[5] (설계도)로 만든 객체의 리모컨을을 넣겠다.
+//
+//
+////     int[]arr = new int[5];  // 위에 두개를 합친 코드
+//
+////     arr[0] = 10;
+////     arr[1] = 20;
+////     arr[2] = 30;
+//
+//        for(int b = 0; b< arr.length; b ++){
+//            arr[b] = (b+1) *10;
+//
+//        }
+//
+////     int sum = arr[0] +arr[1] + arr[2];
+//     int sum = 0;
+//     for (int i = 0 ; i< arr.length; i++){
+//         sum += arr[i];
+//     }
+//     int avg = sum / arr.length;
+////        System.out.println(arr[0]);
+////        System.out.println(arr[1]);
+////        System.out.println(arr[2]);
+////        System.out.printf("첫번째 : %d , 두번째 : %d,세번째 : %d, 총합 : %d" ,arr[0] ,arr[1] , arr[2] , arr[0] +arr[1] + arr[2]);
+//        System.out.println("총합"+ sum);
+//        System.out.println("평균" + avg);
 
-//===================코드업 19문제 해결!!
-
-//Scanner sc = new Scanner(System.in);
-//String a = sc.nextLine();
-//String A[] = a.split("\\.");
-//int b = Integer.parseInt(A[0]);
-//int c = Integer.parseInt(A[1]);
-//int d = Integer.parseInt(A[2]);
-//
-//        System.out.println(String.format("%04d.%02d.%02d", b, c, d));
-//
-
-// ========================코드업 27문제 해결!
-//Scanner sc = new Scanner(System.in);
-//String a = sc.nextLine();
-//String A[] = a.split("\\.");
-//int b = Integer.parseInt(A[0]);
-//int c = Integer.parseInt(A[1]);
-//int d = Integer.parseInt(A[2]);
-//
-//        System.out.println(String.format("%02d-%02d-%02d", d, c , b));
 
 
-// ==========================코드업 28번 문제
-
-//        Scanner sc = new Scanner(System.in);
-//        Long a = sc.nextLong();
-//
-//
-//        System.out.println( a);
-
-//====================코드업 29번 문제
-//
-//Scanner sc = new Scanner(System.in);
-//Double a = sc.nextDouble();
-//
-//        System.out.printf("%.11f", a );
-
-//===================================코드업 30문제
-//Scanner sc = new Scanner(System.in);
-//Long a = sc.nextLong();
-//
-//
-//        System.out.println( a);
+//    boolean []arr1 = new boolean[3];
+//    arr1[0] = true;
+//    arr1[1] = false;
+//    arr1[2] = true;
+//    double []arr2 = new double[3] ;
+//     arr2[0] = 3.14;
+//     arr2[1] = 7.77;
+//     arr2[2] = 11.11;
+//     int []arr3 = new int[10];
+//        System.out.println("arr1:" +"["+arr1[0] +","+ arr1[1]+","+ arr1[2] + "]");
+//        System.out.println("arr2:"+ " ["+arr2[0] +","+ arr2[1]+","+ arr2[2] + "]");
+//    int a = 0;
+//     for(int i = 0; i<arr3.length ; i++){
+//        arr3[i] = (i+1);
+//         System.out.printf("%d", arr3[i] );
+//     }
