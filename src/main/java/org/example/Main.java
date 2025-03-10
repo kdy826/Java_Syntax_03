4e7bb67844db9fcbfb8d2167f49b65cecc8a204b
kdy826 <kimdy66695@gmail.com>
2025-03-10 16:34:50 +0900

    커밋메세지11

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,28 +5,101 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    student kim = new student();
 
-        car sonata = new car();
-        sonata.year = 19;
-        sonata.maxSpeed = 150;
-        sonata.color = "주황색";
+    kim.age = 23;
+    kim.name = "chulsu";
+    kim.solo = true;
+    kim.person();
+    }
+    }
+    class student{
 
-
-        System.out.println(sonata.year +"," + sonata.maxSpeed + ","+ sonata.color);
-
+    String name;
+    int age;
+    boolean solo;
+    void person(){
+        int age = this.age;
+        boolean solo = this.solo;
+        String name = this.name;
+        System.out.printf("나이 : %d\n" ,age); // this.age , age 만 적어도 가능은 하나
+        System.out.printf("이름 :%s\n" , name);// this.age 정도는 적어둘것 생략 가능한 함수
+        System.out.printf("솔로 : %b\n" , solo);
+        }
 
     }
-}
-class car{
-    String model;
-    int year ;
-    String color;
-    int maxSpeed;
+        //=====차 객체
+//        Scanner sc = new Scanner(System.in);
+//
+//        car sonata = new car(); //car 모양의 객체에 리모컨을 넣고 연결
+//        sonata.name = "쏘나타";
+//        sonata.year = 19;
+//        sonata.maxSpeed = 150;
+//        sonata.color = "주황색";
+//
+//        car santapae = new car();
+//        santapae.name = "싼타페";
+//        santapae.year = 10;
+//        santapae.maxSpeed = 180;
+//        santapae.color = "흰색";
+//
+//        System.out.println(sonata.name + "," + sonata.year + "," + sonata.maxSpeed + "," + sonata.color);
+//        System.out.println(santapae.name + "," + santapae.year + "," + santapae.maxSpeed + "," + santapae.color);
+//
+//
+//        player kim = new player();
+//        player.이름 = "kim";
+//        player.나이 = 15;
+//        kim.나이++;
+//        System.out.println(player.나이);
+        //class car {
+//    String name;
+//    String model;
+//    int year;
+//    String color;
+//    int maxSpeed;
+//
+//
+//
+//
+//
+//}
+
+//class player {
+//    static String 이름;
+//    static int 나이;
+//}
 
 
-}
-//     int a = 10;
+// =====사람 , this 불러오는법
+
+//        사람 a사람 = new 사람();
+//        a사람.name = "김철수";
+//        a사람.age = 13;
+//        a사람.introduce();
+//
+//        a사람.name = "김영희";
+//        a사람.age = 34;
+//        a사람.introduce();
+//
+//
+//    }
+//}
+//
+//
+//class 사람{
+//    int age = 22;
+//    String name;
+//    boolean isMarried;
+//        void introduce(){
+//            int age = this.age; // 인스턴스 안에있는 인스턴스 변수의 값을 리모컨에 가져옴
+//            String name = this.name; //고정된값이 아닌 인스턴스 변수에 값을 불러와서 출력함
+//            System.out.println("===자기소개");
+//            System.out.printf("이름 : %s\n" , name);
+//            System.out.printf("나이 : %d\n" , age);
+//    }
+//}
+//     int ;a = 10;
 //
 //     int[] arr = new int[5];
 //
@@ -39,9 +112,9 @@ class car{
 //Scanner sc = new Scanner(System.in);
 //
 //int[] 철수0 = new int[3];
-//// 철수0[0] = 12; -> 데이터의 의미?
-//// 자바에서 기본으로 제공하는 기본 객체타입
-//// 하나의 객체에 여러개의 타입 x
+/// / 철수0[0] = 12; -> 데이터의 의미?
+/// / 자바에서 기본으로 제공하는 기본 객체타입
+/// / 하나의 객체에 여러개의 타입 x
 //
 //IntArr intArr = new IntArr();
 //// 클래스가 정의되어있지않으면 클래스를 직접만든다.
