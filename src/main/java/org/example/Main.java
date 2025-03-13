363b96a1b722732c772b50ff5ee2dfcf490edd66
kdy826 <kimdy66695@gmail.com>
2025-03-13 16:42:49 +0900

    형변환 casting 전사풀이,생성자

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,64 +6,64 @@ import java.util.Scanner;
 public class Main {
 // ===================매우 중요함
     public static void main(String[] args) {
-
-        int i = 로봇.get정수();
-        boolean b = 로봇.get논리();
-        사람 a사람 = 로봇.get사람();
-        사람 a사람2 = 로봇.get사람2();
-        사람 a사람3 = 로봇.get사람3();
-        사람 a사람5 = 로봇.get사람5(123,true); // 5번 중요함
-        로봇.get사람6(123,true); // 5번 중요함
-
-    }
-
-
-}
-class 로봇{
-    static int get정수(){
-
-    return 0;
-    }
-    static boolean get논리(){
-
-    return false;
-    }
-    static 사람 get사람(){
-
-        return null;
-    }
-    static 사람 get사람2(){
-
-        return null;
-    }
-
-    static 사람 get사람3(){
-
-        return null;
-    }
-
-    static 사람 get사람5(int 학번 , boolean 출결){
-        사람 a사람 = new 사람();
-        // 매우중요함
-        a사람.학번 = 학번;
-        a사람.출결 = 출결;
-        return a사람;
-    }
-
-    static void get사람6(int a , boolean b){
-
-
-    }
-
-
-
-
-
-}
-
-class 사람 extends 로봇 {
-
-    int 학번;
-    boolean 출결;
-
-}
+    }}
+//        int i = 로봇.get정수();
+//        boolean b = 로봇.get논리();
+//        사람 a사람 = 로봇.get사람();
+//        사람 a사람2 = 로봇.get사람2();
+//        사람 a사람3 = 로봇.get사람3();
+//        사람 a사람5 = 로봇.get사람5(123,true); // 5번 중요함
+//        로봇.get사람6(123,true); // 5번 중요함
+//
+//    }
+//
+//
+//}
+//class 로봇{
+//    static int get정수(){
+//
+//    return 0;
+//    }
+//    static boolean get논리(){
+//
+//    return false;
+//    }
+//    static 사람 get사람(){
+//
+//        return null;
+//    }
+//    static 사람 get사람2(){
+//
+//        return null;
+//    }
+//
+//    static 사람 get사람3(){
+//
+//        return null;
+//    }
+//
+//    static 사람 get사람5(int 학번 , boolean 출결){
+//        사람 a사람 = new 사람();
+//        // 매우중요함
+//        a사람.학번 = 학번;
+//        a사람.출결 = 출결;
+//        return a사람;
+//    }
+//
+//    static void get사람6(int a , boolean b){
+//
+//
+//    }
+//
+//
+//
+//
+//
+//}
+//
+//class 사람 extends 로봇 {
+//
+//    int 학번;
+//    boolean 출결;
+//
+//}
--- src/main/java/org/example/Main4.java
+++ src/main/java/org/example/Main4.java
@@ -2,120 +2,68 @@ package org.example;
 
 public class Main4 {
     public static void main(String[] args) {
-//        System.out.println("== 표준Tv 리모콘 들여오기 전 ==");
-//
-//        샤오미Tv a샤오미Tv = new 샤오미Tv();
-//        삼성Tv a삼성Tv = new 삼성Tv();
-//        LGTv aLGTv = new LGTv();
-//
-//        a샤오미Tv.켜기();
-//        // 출력 => 샤오미Tv 켜집니다.
-//        a샤오미Tv.끄기();
-//        // 출력 => 샤오미Tv 꺼집니다.
-//        a샤오미Tv.vr켜기();
-//        // 출력 => 샤오미Tv vr켜기!
-//
-//        a삼성Tv.켜기();
-//        // 출력 => 삼성Tv 켜집니다.
-//        a삼성Tv.끄기();
-//        // 출력 => 삼성Tv 꺼집니다.
-//        a삼성Tv.ar켜기();
-//        // 출력 => 삼성Tv ar켜기!
-//
-//        aLGTv.켜기();
-//        // 출력 => LGTv 켜집니다.
-//        aLGTv.끄기();
-//        // 출력 => LGTv 꺼집니다.
-//        aLGTv.게임모드전환();
-//        // 출력 => LGTv 게임모드전환!
-//
-//        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
-//
-//        // 표준Tv 리모콘을 만든다.
-//        표준Tv a표준Tv;
-//
-//        // a표준Tv 변수에 샤오미Tv 객체를 연결한다.
-//        a표준Tv = a샤오미Tv;
-//        a표준Tv.켜기();
-//        // 출력 : 샤오기TV 켜집니다.
-//        a표준Tv.끄기();
-//        // 출력 : 샤오기TV 꺼집니다.
-//
-//        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
-//        a표준Tv = a삼성Tv;
-//        a표준Tv.켜기();
-//        // 출력 : 삼성TV 켜집니다.
-//        a표준Tv.끄기();
-//        // 출력 : 삼성TV 꺼집니다.
-//
-//        // a표준Tv 변수에 LGTv 객체를 연결한다.
-//        a표준Tv = aLGTv;
-//        a표준Tv.켜기();
-//        // 출력 : LGTV 켜집니다.
-//        a표준Tv.끄기();
-//        // 출력 : LGTV 꺼집니다.
-//
-//        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
-//        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
-//        LGTv aLGTv2 = (LGTv)a표준Tv;
-//        aLGTv2.게임모드전환();
-//    }
-//}
-//class 표준Tv{
-//    void 켜기(){
-//
-//    }
-//    void 끄기(){
-//
-//    }
-//}
-//
-//
-//class 샤오미Tv extends 표준Tv {
-//    void 켜기() {
-//        System.out.println("샤오미Tv 켜집니다.");
-//    }
-//
-//    void 끄기() {
-//        System.out.println("샤오미Tv 꺼집니다.");
-//    }
-//
-//    void vr켜기() {
-//        System.out.println("샤오미Tv vr켜기!");
-//    }
-//}
-//
-//class 삼성Tv  extends 표준Tv{
-//    void 켜기() {
-//        System.out.println("삼성Tv 켜집니다.");
-//    }
-//
-//    void 끄기() {
-//        System.out.println("삼성Tv 꺼집니다.");
-//    }
-//
-//    void ar켜기() {
-//        System.out.println("삼성Tv ar켜기!");
-//    }
-//}
-//
-//class LGTv extends 표준Tv {
-//    void 켜기() {
-//        System.out.println("LGTv 켜집니다.");
-//    }
-//
-//    void 끄기() {
-//        System.out.println("LGTv 꺼집니다.");
-//    }
-//
-//    void 게임모드전환() {
-//        System.out.println("LGTv 게임모드전환!");
-//    }
-//}
+        사람 a사람 = new 김철수();
 
 
-// Tv aTv1 = new LGTv(); // 티비들의 부모 Tv 객체는
-// Tv aTv1 = new LGTv(); // 티비들의 부모 Tv 객체는
-// Tv aTv1 = new LGTv(); // 티비들의 부모 Tv 객체는
+        System.out.println("===사람정보");
+        System.out.printf("이름: %s\n", a사람.이름);
+        System.out.printf("나이: %d\n", a사람.나이);
 
-    }}
+
+        사람 b사람 = new 사람();
+
+        b사람.이름 = "이영희";
+        System.out.println("====사람정보");
+        System.out.printf("이름: %s\n", b사람.이름);
+        System.out.printf("나이: %d\n", b사람.나이);
+
+        전사 a = new  전사();
+    }
+
+
+}
+class 전사{
+
+}
+
+class 사람 { //생성자는 클래스타입과 같은이름을  가져감
+    String 이름;
+    int 나이;
+
+//     void 나이를 _22_살_로설정(){ //수동
+//          this.나이 = 22;
+//    }
+    사람(){  //생성자 자동
+          System.out.println("사람 나이 생성자 실행됨");
+          this.나이 = 22;
+    }
+    사람(String 이름){  //생성자 자동
+        System.out.println("사람 이름 생성자 실행됨");
+        this.이름 = 이름;
+    }
+
+}
+class 김철수 extends 사람{
+   김철수(){
+       System.out.println("김철수 생성됨");
+       this.이름 = " 김철수";
+   }
+}
+
+
+//     인간 a사람 = new 인간();
+//     a사람.나이 = 22;
+//     a사람.왼팔 = new 팔(); // 클래스 팔 생성
+//     a사람.왼팔.길이 = 10;  //a사람으로부터 인스턴스변수 왼팔에서
+//                            // 팔객체 길이까지 두번이동
+//
+//    }
+//
+//}
+//class 인간{
+//    int 나이;
+//    팔 왼팔;  //인스턴스 변수에는 팔의 리모콘 왼팔을 넣는다.
+//}
+//class 팔{
+//   int 길이;
+//}
